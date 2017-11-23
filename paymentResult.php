00ddde166975293b78b6082d43e7b8367f18fde1
Dmitry Vigranovsky <Dmitry_Vigranovsky@epam.com>
2017-11-23 14:23:38 +0200

    check real payment

--- paymentResult.php
+++ paymentResult.php
@@ -10,12 +10,15 @@
 	$sign = base64_encode(sha1($private_key.$data.$private_key,1));
 	
 	if($signature === $sign) {
-		$data = base64_decode($data);
+		$data = json_encode(base64_decode($data));
 		var_dump($data);
+		$strToCrm = 'Status:'.$data['status'].' Amount:'.$data['amount'].' OrderId:'.$data['order_id'].' Description:'.$data['description'];
 	} else {
 		echo 'Signatures is not equal';
 	}
 	//var_dump($_POST);
+	
+	echo $strToCrm;
 	
 	$result = ob_get_clean();
 	echo file_put_contents ('1.txt', $result);
