ede3f43de9177b0d5c3ebae3d4b3b39adb42ef96
Dmitry Vigranovsky <Dmitry_Vigranovsky@epam.com>
2017-11-23 14:09:23 +0200

    payment result

--- paymentResult.php
+++ paymentResult.php
@@ -1,8 +1,22 @@
 <?
 	//https://www.liqpay.ua/documentation/api/callback
 	ob_start();
-	var_dump($_GET);
-	var_dump($_POST);
+	
+	$private_key = 'i79786110481';
+	
+	$data = $_POST['data'];
+	$signature = $_POST['signature'];
+	
+	$sign = base64_encode(sha1($private_key.$data.$private_key,1));
+	
+	if($signature === $sign) {
+		$data = base64_decode($data);
+		var_dump($data);
+	} else {
+		echo 'Signatures is not equal';
+	}
+	//var_dump($_POST);
+	
 	$result = ob_get_clean();
 	echo file_put_contents ('1.txt', $result);
 ?>
