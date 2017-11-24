3c4195acedcc7c142daece9ad045828e689c568f
alexs <a.sharabudinov@twinwingames.com>
2017-11-24 12:07:56 +0200

    [payment] - payment prod mode (sandbox disabled)

--- payjs.php
+++ payjs.php
@@ -2,17 +2,17 @@
 
 	$amount = (float) $_GET['amount'];
 	$description = $_GET['description'];
-	
+
 	if($amount == 0) {
 		echo '//need amount field';
 		exit();
 	}
-	
+
 	if(strlen($description) < 3) {
 		echo '//need description field. min lenght 3';
 		exit();
 	}
-	
+
 	$data = array(
 		'action'         => 'pay',
 		'amount'         => $amount,
@@ -20,10 +20,10 @@
 		'description'    => $description,
 		'order_id'       => 'order_'.time(),
 		'version'        => '3',
-		'sandbox' => 1, //TODO delete row for real payments
+		//'sandbox' => 1, //TODO delete row for real payments
 		'server_url' => 'https://silkepil.com.ua/payment-result'
 	);
-	
+
 	//$('head').append('<script src="https://silkepil.com.ua/pay.js?amount=1.2&description='+encodeURI('Дескріпш який баче юзер коли проплачує')+'&time='+Date.now()+'"></script>');
 	//https://www.liqpay.ua/documentation/api/aquiring/widget/doc
 	require_once('LiqPay.php');
